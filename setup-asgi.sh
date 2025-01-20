33a7cd06a7f86b2ebcc68aec9e325b9d432ae398
Dmitry Zakharov <dmitry.zakharov@caelus-technologies.com>
2025-01-20 18:41:31 +0000

    fastapi issue'

--- setup-asgi.sh
+++ setup-asgi.sh
@@ -6,8 +6,9 @@ dir=$(dirname $(readlink -f $0))
 
 sudo systemctl start unit
 
+yq -i ".applications.python.path=\"$dir\"" setup-asgi.json
+yq -i ".applications.python.home=\"${dir}/.venv/\"" setup-asgi.json
 
-sudo cp *.py /var/log/unit/
 
 sudo curl -X PUT --data-binary "$(cat $dir/setup-asgi.json)" \
 --unix-socket /var/run/unit/control.sock http://localhost/config/
