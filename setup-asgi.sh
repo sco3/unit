9299198a2abefe6198348f85cbbc3077026cff73
Dmitry Zakharov <dmitry.zakharov@caelus-technologies.com>
2025-01-20 18:22:41 +0000

    upd activity timestamp

--- setup-asgi.sh
+++ setup-asgi.sh
@@ -7,7 +7,7 @@ dir=$(dirname $(readlink -f $0))
 sudo systemctl start unit
 
 
-sudo cp unit_asgi.py /var/log/unit/
+sudo cp *.py /var/log/unit/
 
 sudo curl -X PUT --data-binary "$(cat $dir/setup-asgi.json)" \
 --unix-socket /var/run/unit/control.sock http://localhost/config/
--- setup-wsgi.sh
+++ setup-wsgi.sh
@@ -7,9 +7,9 @@ dir=$(dirname $(readlink -f $0))
 sudo systemctl start unit
 
 
-sudo cp unit_wsgi.py /var/log/unit/
+sudo cp *.py /var/log/unit/
 
-sudo curl -X PUT --data-binary "$(cat $dir/setup.json)" \
+sudo curl -X PUT --data-binary "$(cat $dir/setup-wsgi.json)" \
 --unix-socket /var/run/unit/control.sock http://localhost/config/
 
 sudo systemctl restart unit
