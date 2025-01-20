5ecde214bf668b3ca240cde23435305117502492
Dmitry Zakharov <dmitry.zakharov@caelus-technologies.com>
2025-01-20 19:01:38 +0000

    wsgi works, fastapi fails

--- setup-wsgi.sh
+++ setup-wsgi.sh
@@ -6,8 +6,12 @@ dir=$(dirname $(readlink -f $0))
 
 sudo systemctl start unit
 
+yq -i ".applications.python.path=\"${dir}/\"" setup-asgi.json
+yq -i ".applications.python.home=\"${dir}/.venv/\"" setup-asgi.json
+yq -i ".applications.python.working_directory=\"${dir}/\"" setup-asgi.json
 
-sudo cp *.py /var/log/unit/
+
+#sudo cp *.py /var/log/unit/
 
 sudo curl -X PUT --data-binary "$(cat $dir/setup-wsgi.json)" \
 --unix-socket /var/run/unit/control.sock http://localhost/config/
