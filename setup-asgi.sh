ac7b0e6af794b0282729a757e9b3151137278dd6
Dmitry Zakharov <dmitry.zakharov@caelus-technologies.com>
2025-01-20 18:57:33 +0000

    asgi issue

--- setup-asgi.sh
+++ setup-asgi.sh
@@ -6,11 +6,11 @@ dir=$(dirname $(readlink -f $0))
 
 sudo systemctl start unit
 
-yq -i ".applications.python.path=\"$dir\"" setup-asgi.json
+yq -i ".applications.python.path=\"${dir}/\"" setup-asgi.json
 yq -i ".applications.python.home=\"${dir}/.venv/\"" setup-asgi.json
+yq -i ".applications.python.working_directory=\"${dir}/\"" setup-asgi.json
 
-
-sudo curl -X PUT --data-binary "$(cat $dir/setup-asgi.json)" \
+sudo curl -X PUT --data-binary @setup-asgi.json \
 --unix-socket /var/run/unit/control.sock http://localhost/config/
 
 sudo systemctl restart unit
