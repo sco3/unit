41ba324c822fec911dd56eb38a5810caf06b8eec
Dmitry Zakharov <zakharov@ie.ibm.com>
2025-01-21 18:18:06 -0500

    asgi works, slowly than wsgi

--- setup-asgi.sh
+++ setup-asgi.sh
@@ -6,15 +6,18 @@ dir=$(dirname $(readlink -f $0))
 
 sudo systemctl start unit
 
-yq -i ".applications.python.path=\"${dir}/\"" setup-asgi.json
-yq -i ".applications.python.home=\"${dir}/.venv/\"" setup-asgi.json
-yq -i ".applications.python.working_directory=\"${dir}/\"" setup-asgi.json
+yq -i -P -o json ".applications.python.path=\"${dir}/\"" setup-asgi.json
+yq -i -P -o json ".applications.python.home=\"${dir}/.venv/\"" setup-asgi.json
+yq -i -P -o json ".applications.python.working_directory=\"${dir}/\"" setup-asgi.json
 
 sudo curl -X PUT --data-binary @setup-asgi.json \
 --unix-socket /var/run/unit/control.sock http://localhost/config/
 
 sudo systemctl restart unit
 
+
 echo ============================
 echo check logs in /var/log/unit/
+
+sudo tail -f -n 40 /var/log/unit/unit.lo
 
--- unit_asgi_ms.py
+++ unit_asgi_ms.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env -S uv run
 
-#import uvicorn
+import uvicorn
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello, Fastapi!"}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "unit_asgi_ms:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=False,
+        log_level="error",
+    )
+
+
+
 
